16bd948f2b57611f7b03cbcf438f1c34b6516fad
Mindaugas Sandanavicius <m.sandanavicius@gmail.com>
2025-02-19 02:49:17 +0200

    Add header and footer

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/ui/button';
+import { APP_NAME } from '@/lib/constants';
+import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -7,8 +10,29 @@ const Header = () => {
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className="flex-start">
-            <Image src="/images/logo.svg" />
+            <Image
+              src=""
+              alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
           </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <UserIcon /> Sign in
+            </Link>
+          </Button>
         </div>
       </div>
     </header>
